1191c9eea4e79637f93e8651228ba9554be6fd2b
Markus Heidelberg <markus.heidelberg@web.de>
2015-07-16 23:51:08 +0200

    Allow exporting Notes in addition to the OSM map data
    The data type can be chosen via selection list, default is OSM XML as
    before.

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -13,6 +13,22 @@ class ExportController < ApplicationController
       # redirect to API map get
       redirect_to "http://api.openstreetmap.org/api/#{API_VERSION}/map?bbox=#{bbox}"
 
+    elsif format == "notes_rss"
+      # redirect to API notes get
+      redirect_to "http://api.openstreetmap.org/api/#{API_VERSION}/notes.rss?bbox=#{bbox}&limit=10000"
+
+    elsif format == "notes_xml"
+      # redirect to API notes get
+      redirect_to "http://api.openstreetmap.org/api/#{API_VERSION}/notes.xml?bbox=#{bbox}&limit=10000"
+
+    elsif format == "notes_json"
+      # redirect to API notes get
+      redirect_to "http://api.openstreetmap.org/api/#{API_VERSION}/notes.json?bbox=#{bbox}&limit=10000"
+
+    elsif format == "notes_gpx"
+      # redirect to API notes get
+      redirect_to "http://api.openstreetmap.org/api/#{API_VERSION}/notes.gpx?bbox=#{bbox}&limit=10000"
+
     elsif format == "mapnik"
       # redirect to a special 'export' cgi script
       format = params[:mapnik_format]
